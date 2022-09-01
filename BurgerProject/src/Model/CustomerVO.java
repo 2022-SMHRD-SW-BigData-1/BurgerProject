484bcf72652b56d503e481956948055b66be3fa7
EGEG22A07 <EGEG22A07@DESKTOP-LV5L344>
2022-09-01 17:25:22 +0900

    customerVO 재수정 - 민유현

--- BurgerProject/src/Model/CustomerVO.java
+++ BurgerProject/src/Model/CustomerVO.java
@@ -4,13 +4,13 @@ public class CustomerVO {
 
 	private String customer;
 	private String dialogue;
-	private String recepie;
+
 	
 	public CustomerVO(String customer, String dialogue, String recepie) {
 		super();
 		this.customer = customer;
 		this.dialogue = dialogue;
-		this.recepie = recepie;
+
 		
 		
 	}
@@ -29,14 +29,6 @@ public class CustomerVO {
 
 	public void setDialogue(String dialogue) {
 		this.dialogue = dialogue;
-	}
-	
-	public String getRecepie() {
-		return recepie;
-	}
-
-	public void setRecepie(String recepie) {
-		this.recepie = recepie;
 	}
 	
 }
