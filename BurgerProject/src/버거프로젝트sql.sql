ac007b716700d5026ccf16cce7b9de3484dcfa2c
smhrd <smhrd@DESKTOP-4SKOSJL>
2022-09-01 18:13:32 +0900

    "데이터베이스 구축완료!" -조찬익

--- BurgerProject/src/버거프로젝트sql.sql
+++ BurgerProject/src/버거프로젝트sql.sql
@@ -1,22 +1,23 @@
-create table memberInfo(
-id varchar2(30)
-constraint memberInfo_id_pk primary key
-constraint memberInfo_id_nn not null,
-password varchar2(30)
-constraint memberInfo_password_nn not null,
-nickname varchar2(30)
-constraint memberInfo_nickname_uk unique
-constraint memberInfo_nickname_nn not null,
-score number(20)
-constraint memberInfo_score_uk unique
-constraint memberInfo_score_nn not null);
+create table Score1
+(
+score number(20) not null
+constraint Score1_score_pk primary key
+);
 
-create table Ranking(
-nickname varchar2(30)
-constraint Ranking_nickname_fk references memberInfo(nickname)
-constraint Ranking_nickname_nn not null,
-score number(30)
-constraint Ranking_score_fk references memberInfo(score)
-constraint Ranking_score_nn not null);
+create table memberInfo
+(
+id varchar2(20) not null
+constraint memberInfo_id_pk primary key,
+password varchar2(20) not null,
+nickname varchar2(20) not null,
+score number(20) not null,
+constraint fk_memberInfo_score foreign key(score) references Score1(score)
+);
 
-select * from memberInfo;
+create table Ranking
+(
+id varchar2(10) not null,
+score number(20) not null,
+constraint Ranking_id_fk foreign key(id) references memberInfo(id),
+constraint Ranking_score_fk foreign key(score) references Score1(score)
+);
