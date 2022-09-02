a67a104a1c661fbd5a1a0124edba003fbae19eea
EGEG22A03 <EGEG22A03@DESKTOP-00OP262>
2022-09-02 09:14:59 +0900

    버거스토어 - 찬호

--- BurgerProject/src/View/BurgerStore.java
+++ BurgerProject/src/View/BurgerStore.java
@@ -5,16 +5,20 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import CTRL.DAO;
+import CTRL.ingredientCTRLMethod;
 
 public class BurgerStore {
 	static DAO dao = new DAO();
 	static Scanner sc = new Scanner(System.in);
+	static int i = 1;
 
 	public static void main(String[] args) {
 		startLogo();
 		User();
 		start();
 		selectLevel();
+		System.out.println("로딩..............");
+		play();
 
 	}
 
@@ -51,7 +55,9 @@ public class BurgerStore {
 				String pw = sc.next();
 				int cnt = dao.login(id, pw);
 				if (cnt == 1) {
+					escapeN(32);
 					System.out.println("\t\t\t\t로그인 성공!\n");
+
 					break;
 				} else if (cnt == 0) {
 					System.out.println("\t\t\t\t비밀번호가 틀렸습니다.");
@@ -78,9 +84,15 @@ public class BurgerStore {
 	}
 
 	public static void start() {
-		System.out.println("\t\t\t    환영합니다! 당신은 BurgerKing의 담당 쉐프입니다.\r\n"
-				+ "\t\t\t    우리 식당의 규칙은 간단해요! FastFood인 만큼 빠르고! 맛있게! 손님들을 대접한다!\r\n"
-				+ "\t\t\t    정말 쉽죠? 더이상의 설명은 생략한다. 진정한 쉐프생활은 맞아가면서 배우는법!!\n\n");
+		System.out.println("\t\t\tAI가 세상을 지배한지 벌써 20년이나 지났다.. 인류 절반이 타노스 당했고 AI의 인간통제와\r\n"
+				+ "\t\t\t문명말살로 인해 인간의 컴퓨터는 도스창을 사용할 수 밖에 없는 세상이 왔다.\r\n"
+				+ "\t\t\t수 많은 인간의 음식들이 사라졌고 유일하게 남은 요식업 기업이 바로 BURGER KING...\r\n"
+				+ "\t\t\t생존한 인간들은 외식을 할때면 BURGER KING의 버거만 먹어야 하는데...\r\n"
+				+ "\t\t\t항상 똑같은 메뉴에 질린 인간들은 분노하여 BURGER KING에 올때면 자기들 마음대로 레시피를 부르곤 한다.\r\n"
+				+ "\t\t\t(여기가 무슨 써브웨인줄 아나..)\r\n"
+				+ "\t\t\t그러다 갑자기 뼈저리게 가난한 우리의 주인공 \"너님\"은 우연히 비어있는 BURGER KING 점포를 발견하고\r\n"
+				+ "\t\t\t번뜩이는 아이디어를 떠올리며 들어가게 되는데.................\n\n");
+
 	}
 
 	public static void selectLevel() {
@@ -91,17 +103,17 @@ public class BurgerStore {
 		switch (nu) {
 		case 1: {
 			System.out.println("\t\t\t\tEasy 난이도를 선택하셨습니다. 3초 후 게임을 시작하겠습니다.");
-			startCountdown(1);
+			startCountdown(3);
 			break;
 		}
 		case 2: {
 			System.out.println("\t\t\t\tNormal 난이도를 선택하셨습니다. 3초 후 게임을 시작하겠습니다.");
-			startCountdown(1);
+			startCountdown(3);
 			break;
 		}
 		case 3: {
 			System.out.println("\t\t\t\tHard 난이도를 선택하셨습니다. 3초 후 게임을 시작하겠습니다.");
-			startCountdown(1);
+			startCountdown(3);
 			break;
 		}
 		default:
@@ -113,15 +125,12 @@ public class BurgerStore {
 	public static void startCountdown(int count) {
 
 		Timer timer = new Timer();
-
 		TimerTask task = new TimerTask() {
-			int i = 3;
-			
 			@Override
 			public void run() {
-				if (i >= count) {
-					System.out.println("\t\t\t\t[SYSTEM] 카운트다운 : " + i);
-					i--;
+				if (i <= count) {
+					System.out.println("\t\t\t\t[SYSTEM] 카운트다운 : " + (count+1-i));
+					i++;
 				} else {
 					System.out.println("\t\t\t\t[SYSTEM] 시작!");
 					timer.cancel();
@@ -131,6 +140,7 @@ public class BurgerStore {
 
 		};
 		timer.schedule(task, 1000, 1000);
+
 	}
 
 	public static void startLogo() {
@@ -172,6 +182,51 @@ public class BurgerStore {
 						+ "                                                                                                    \r\n"
 						+ "                                                                                                    \r\n"
 						+ "                                                                                                    ");
+	}
+
+	public static void escapeN(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println();
+		}
+	}
+
+	public static void play() {
+		ingredientCTRLMethod Method = new ingredientCTRLMethod();
+		long start = System.currentTimeMillis();
+		long time = 3*60*10;
+		while ((System.currentTimeMillis() - start) < time) {
+			PrintMenu();
+			System.out.print("<선택> : ");
+			int choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				Method.BuyIngredient();
+				break;
+//		case 2:
+//			manager.deposit();
+//			break;
+//		case 3:
+//			manager.withdraw();
+//			break;
+//		case 4:
+//			manager.inquire();
+//			break;
+//		case 5:
+//			manager.disp();
+//			break;
+			default:
+				System.out.println("잘못입력하셨습니다. 재입력해주세요.");
+				break;
+			}
+		}
+
+	}
+
+	public static void PrintMenu() {
+		System.out.println();
+		System.out.println("======================= Menu ========================");
+		System.out.println("[1]재료구매 [2]주방 및 제작 [3]햄버거 판매 [4]재료 재고파악 [5]포기");
 	}
 
 }
