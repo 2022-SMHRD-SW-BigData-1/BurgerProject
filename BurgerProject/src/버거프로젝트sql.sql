1fa5bc9dc06aa6d906bd67d9d6c0406123a020a0
smhrd <smhrd@DESKTOP-4SKOSJL>
2022-09-02 09:13:26 +0900

    "데이터베이스 최종컨펌 통과하게 해주세요 하느님..." -조찬익

--- BurgerProject/src/버거프로젝트sql.sql
+++ BurgerProject/src/버거프로젝트sql.sql
@@ -1,23 +1,11 @@
-create table Score1
-(
-score number(20) not null
-constraint Score1_score_pk primary key
-);
-
-create table memberInfo
-(
-id varchar2(20) not null
+create table memberInfo(
+id varchar2(30) not null
 constraint memberInfo_id_pk primary key,
-password varchar2(20) not null,
-nickname varchar2(20) not null,
-score number(20) not null,
-constraint fk_memberInfo_score foreign key(score) references Score1(score)
-);
+password varchar2(30) not null,
+nickname varchar2(30) not null);
 
-create table Ranking
-(
-id varchar2(10) not null,
-score number(20) not null,
-constraint Ranking_id_fk foreign key(id) references memberInfo(id),
-constraint Ranking_score_fk foreign key(score) references Score1(score)
-);
+drop table memberInfo;
+
+create table Ranking(
+nickname varchar2(30) not null,
+score number(20) not null);
