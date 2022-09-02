8f385b90b736dee7c67c6561b6056ac80f1b6ce7
smhrd <smhrd@DESKTOP-3ACBNCG>
2022-09-02 11:46:22 +0900

    홍진영 - BurgerStore에 메소드 등록 및 재료추가메소드 수정

--- BurgerProject/src/View/BurgerStore.java
+++ BurgerProject/src/View/BurgerStore.java
@@ -1,10 +1,12 @@
 package View;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import CTRL.DAO;
+import CTRL.ingredientCTRL;
 import CTRL.ingredientCTRLMethod;
 
 public class BurgerStore {
@@ -103,17 +105,17 @@ public class BurgerStore {
 		switch (nu) {
 		case 1: {
 			System.out.println("\t\t\t\tEasy 난이도를 선택하셨습니다. 곧 게임을 시작하겠습니다.");
-			
+
 			break;
 		}
 		case 2: {
 			System.out.println("\t\t\t\tNormal 난이도를 선택하셨습니다. 곧 게임을 시작하겠습니다.");
-			
+
 			break;
 		}
 		case 3: {
 			System.out.println("\t\t\t\tHard 난이도를 선택하셨습니다. 곧 게임을 시작하겠습니다.");
-			
+
 			break;
 		}
 		default:
@@ -129,7 +131,7 @@ public class BurgerStore {
 			@Override
 			public void run() {
 				if (i <= count) {
-					System.out.println("\t\t\t\t[SYSTEM] 카운트다운 : " + (count+1-i));
+					System.out.println("\t\t\t\t[SYSTEM] 카운트다운 : " + (count + 1 - i));
 					i++;
 				} else {
 					System.out.println("\t\t\t\t[SYSTEM] 시작!");
@@ -193,40 +195,178 @@ public class BurgerStore {
 	public static void play() {
 		ingredientCTRLMethod Method = new ingredientCTRLMethod();
 		long start = System.currentTimeMillis();
-		long time = 3*60*10;
+		long time = 3 * 60 * 1000;
 		while ((System.currentTimeMillis() - start) < time) {
 			PrintMenu();
 			System.out.print("<선택> : ");
 			int choice = sc.nextInt();
 
-			switch (choice) {
-			case 1:
-				Method.BuyIngredient();
-				break;
-//		case 2:
-//			manager.deposit();
-//			break;
-//		case 3:
-//			manager.withdraw();
-//			break;
-//		case 4:
-//			manager.inquire();
-//			break;
-//		case 5:
-//			manager.disp();
-//			break;
-			default:
-				System.out.println("잘못입력하셨습니다. 재입력해주세요.");
-				break;
-			}
-		}
+			if (choice == 1) {
+				BuyIngredient();
+			} else if (choice == 2) {
 
+			} else if (choice == 3) {
+
+			} else if (choice == 4) {
+				CheckIngredient();
+			} else if (choice == 5) {
+				System.out.println("게임을 포기하셨습니다.");
+			} else {
+				System.out.println("잘못입력하셨씁니다. 재입력해주세요.");
+			}
+
+		}
 	}
 
 	public static void PrintMenu() {
 		System.out.println();
 		System.out.println("======================= Menu ========================");
 		System.out.println("[1]재료구매 [2]주방 및 제작 [3]햄버거 판매 [4]재료 재고파악 [5]포기");
+	}
+
+	private static ArrayList<ingredientCTRL> CTRL;
+
+	public void ingredientCTRLMethod() {
+		CTRL = new ArrayList<ingredientCTRL>();
+	}
+
+	static int Cntmeatpatty = 0;
+	static int Cntshrimppatty = 0;
+	static int Cntchickenpatty = 0;
+	static int Cntbacon = 0;
+
+	static int Cnttomato = 0;
+	static int Cntpickle = 0;
+	static int Cntcabbage = 0;
+
+	static int Cntbread = 0;
+	static int Cntmustard = 0;
+	static int Cntketchup = 0;
+	static int Cntcheese = 0;
+
+	// [1] 재료구매
+
+	public static void BuyIngredient() {
+		while (true) {
+			ingredientCTRL igCTRL = new ingredientCTRL();
+			System.out.println("[1]육류 [2]채소류 [3]기타류 [4]처음화면으로 이동");
+			System.out.print("<선택> : ");
+			int num1 = sc.nextInt();
+
+			if (num1 == 1) {
+				System.out.println("[1]고기패티 [2]새우패티 [3]치킨패티 [4]베이컨 [5]이전");
+				System.out.print("<선택> : ");
+				int num2 = sc.nextInt();
+
+				switch (num2) {
+				case 1:
+					System.out.println("고기패티를 구매하였습니다.");
+					System.out.println();
+					Cntmeatpatty++;
+					break;
+				case 2:
+					System.out.println("새우패티를 구매하였습니다.");
+					System.out.println();
+					Cntshrimppatty++;
+					break;
+				case 3:
+					System.out.println("치킨패티를 구매하였습니다.");
+					System.out.println();
+					Cntchickenpatty++;
+					break;
+				case 4:
+					System.out.println("베이컨를 구매하였습니다.");
+					System.out.println();
+					Cntbacon++;
+					break;
+				case 5:
+					System.out.println("이전으로 돌아가겠습니다.");
+					System.out.println();
+					continue;
+				}
+
+			}
+
+			if (num1 == 2) {
+				System.out.println("[1]토마토 [2]피클 [3]양상추 [4]이전");
+				System.out.print("<선택> : ");
+				int num3 = sc.nextInt();
+
+				switch (num3) {
+				case 1:
+					System.out.println("토마토를 구매하였습니다.");
+					System.out.println();
+					Cnttomato++;
+					break;
+				case 2:
+					System.out.println("피클을 구매하였습니다.");
+					System.out.println();
+					Cntpickle++;
+					break;
+				case 3:
+					System.out.println("양상추를 구매하였습니다.");
+					System.out.println();
+					Cntcabbage++;
+					break;
+				case 4:
+					System.out.println("이전으로 돌아가겠습니다.");
+					System.out.println();
+					continue;
+				}
+			}
+
+			if (num1 == 3) {
+				System.out.println("[1]빵 [2]머스타드 [3]케찹 [4]치즈 [5]이전");
+				System.out.print("<선택> : ");
+				int num4 = sc.nextInt();
+
+				switch (num4) {
+				case 1:
+					System.out.println("빵을 구매하였습니다.");
+					System.out.println();
+					Cntbread++;
+					break;
+				case 2:
+					System.out.println("머스타드을 구매하였습니다.");
+					System.out.println();
+					Cntmustard++;
+					break;
+				case 3:
+					System.out.println("케찹을 구매하였습니다.");
+					System.out.println();
+					Cntketchup++;
+					break;
+				case 4:
+					System.out.println("치즈를 구매하였습니다.");
+					System.out.println();
+					Cntcheese++;
+					break;
+				case 5:
+					System.out.println("이전으로 돌아가겠습니다.");
+					System.out.println();
+					continue;
+				}
+
+			}
+
+			if (num1 == 4) {
+				System.out.println("처음화면으로 이동합니다.");
+				System.out.println();
+				break;
+			}
+
+		}
+
+	}
+
+	// [4] 재료 재고파악
+
+	public static void CheckIngredient() {
+		System.out.println("[1]고기패티 : " + Cntmeatpatty + "개, [2]새우패티 : " + Cntshrimppatty + "개, [3]치킨패티 : "
+				+ Cntchickenpatty + "개, [4]베이컨 : " + Cntbacon + "개");
+		System.out.println("[1]토마토 : " + Cnttomato + "개, [2]피클 : " + Cntpickle + "개, [3]양상추 : " + Cntcabbage + "개");
+		System.out.println("[1]빵 : " + Cntbread + "개, [2]머스타드 : " + Cntmustard + "개, [3]케찹 : " + Cntketchup
+				+ "개, [4]치즈 : " + Cntcheese + "개");
 	}
 
 }
