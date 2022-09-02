b95f857db7f6524606421b1556c48a243b5be27c
smhrd <smhrd@DESKTOP-4SKOSJL>
2022-09-02 09:49:43 +0900

    "customermain과 customermethod 구현했습니다!" - 민유현 조찬익

---
+++ BurgerProject/src/CTRL/customerMAIN.java
@@ -0,0 +1,12 @@
+package CTRL;
+
+public class customerMAIN {
+
+	public static void main(String[] args) {
+		customerMETHOD cus = new customerMETHOD();
+		cus.customer();
+		cus.dialogue();
+
+	}
+
+}
